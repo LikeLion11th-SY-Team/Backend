244db0222e16c7244ed87da9d2d38834dfc1db17
newmin0119@gmail.com <>
2023-08-04 14:49:26 +0900

    FEAT 닉네임 api

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import UserAPIView, SpartaTokenObtainPairView,SignupView
 from .views import checkDuplicatedID,checkDuplicatedNickname
+from .views import getNickname
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 app_name = "users"
@@ -11,6 +12,7 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/sparta/token/',SpartaTokenObtainPairView.as_view(),name='sparta_token'),
-    path('api/checkID/',checkDuplicatedID,name='check_id'),
-    path('api/checkUsername/',checkDuplicatedNickname,name='check_Name'),
+    path('api/check/id/',checkDuplicatedID,name='check_id'),
+    path('api/check/nickname/',checkDuplicatedNickname,name='check_Name'),
+    path('api/get/nickname/',getNickname,name='get_nickname'),
 ]
