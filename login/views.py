84fed911308821c51c030d2332617cbd38f2fc41
JSHWJ <bogum1029@gmail.com>
2023-08-06 15:49:36 +0900

    [FEAT] 카카오 로그인 구현 (#1)

--- login/views.py
+++ login/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework import status
+# from rest_framework_simplejwt.tokens import RefreshToken
+# from allauth.socialaccount.models import SocialAccount
+# # from user.models import User as UserModel
 
 # Create your views here.
