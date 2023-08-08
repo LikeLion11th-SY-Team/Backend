5207f260339ed88e934637ce5be3894701ea7db3
JSHWJ <bogum1029@gmail.com>
2023-08-08 12:45:15 +0900

    [FIX] views 파일 수정 (#1)
    - 카카오 로그인 로직 fix 및 랜덤 닉네임 생성 로직 add

--- login/views.py
+++ login/views.py
@@ -1,45 +1,50 @@
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
-# from rest_framework import status
-# from rest_framework_simplejwt.tokens import RefreshToken
-# from allauth.socialaccount.models import SocialAccount
-# # from user.models import User as UserModel
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+import requests
+from rest_framework_simplejwt.tokens import RefreshToken
+# from .models import User
+import random
+import string
 
-# class KakaoLoginView(APIView):
+def generate_random_nickname():
+    # 랜덤한 문자열을 생성하여 닉네임으로 사용
+    letters = string.ascii_letters
+    random_nickname = ''.join(random.choice(letters) for i in range(10))  # 10자리 랜덤 닉네임 생성
+    return random_nickname
 
-#     def post(self, request):
-#         email = request.data.get("email")
+class KakaoLoginView(APIView):
 
-#         try:
-#             # 기존에 가입된 유저와 쿼리해서 존재하면서, socialaccount에도 존재하면 로그인
-#             # user = UserModel.objects.get(email=email) => db 테이블 이름을 아직 정하지 않음
-#             social_user = SocialAccount.objects.filter(user=user).first()
+    def get_user_info(self, access_token):
+        headers = {'Authorization': f'Bearer {access_token}'}
+        response = requests.get('https://kapi.kakao.com/v2/user/me', headers=headers)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
 
-#             if social_user:
-#                 # 카카오로 가입된 유저가 로그인하는 경우
-#                 if social_user.provider == "kakao":
-#                     # JWT Access Token 발급
-#                     refresh = RefreshToken.for_user(user)
-#                     return Response({'refresh': str(refresh), 'access': str(refresh.access_token), "msg": "로그인 성공"}, status=status.HTTP_200_OK)
-#                 else:
-#                     # 카카오로 가입되지 않은 유저가 카카오로 로그인하려는 경우 에러 반환
-#                     return Response({"error": "카카오로 가입된 유저가 아닙니다. 카카오로 로그인을 시도해주세요."}, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request):
+        access_token = request.data.get('access_token')
 
-#             else:
-#                 # 이메일로 가입된 유저가 카카오로 로그인하려는 경우 에러 반환
-#                 return Response({"error": "이메일로 가입된 유저가 아닙니다. 이메일로 로그인을 시도해주세요."}, status=status.HTTP_400_BAD_REQUEST)
+        user_info = self.get_user_info(access_token)
+        if user_info:
+            social_id = user_info.get('id')
 
-#         except UserModel.DoesNotExist:
-#             # 이메일로 가입되지 않은 유저가 카카오로 로그인하려는 경우 카카오 회원가입 로직으로 이동
-#             # 카카오 회원가입을 위한 로직 구현
-#             nickname = generate_random_nickname()  # 랜덤 닉네임 생성
-#             new_user = UserModel.objects.create(
-#                 nickname=nickname,  # 랜덤 닉네임 사용
-#                 email=email,
-#             )
-#             SocialAccount.objects.create(
-#                 user_id=new_user.id,
-#             )
-#             # JWT Access Token 발급
-#             refresh = RefreshToken.for_user(new_user)
-#             return Response({'refresh': str(refresh), 'access': str(refresh.access_token), "msg": "카카오 회원가입 및 로그인 성공"}, status=status.HTTP_201_CREATED)
+            try:
+                user = User.objects.get(social_id=social_id)
+
+                refresh = RefreshToken.for_user(user)
+                return Response({'refresh': str(refresh), 'access': str(refresh.access_token), "msg": "로그인 성공"}, status=status.HTTP_200_OK)
+
+            except User.DoesNotExist:
+                nickname = generate_random_nickname()  # 랜덤 닉네임 생성
+                new_user = User.objects.create(
+                    nickname=nickname, 
+                    social_id=social_id,  # 카카오 소셜 ID 저장
+                )
+
+                refresh = RefreshToken.for_user(new_user)
+                return Response({'refresh': str(refresh), 'access': str(refresh.access_token), "msg": "카카오 회원가입 및 로그인 성공"}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"error": "유효하지 않은 액세스 토큰"}, status=status.HTTP_401_UNAUTHORIZED)
+
