da28a686551ca6a19748fbda9f5cd5d4f65d473f
newmin0119@gmail.com <>
2023-08-10 17:25:11 +0900

    FEAT Community 모델

--- posts/models.py
+++ posts/models.py
@@ -3,13 +3,15 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Post(models.Model):
-    writer = models.ForeignKey(to=User,on_delete=models.CASCADE)
+    writer = models.ForeignKey(User,on_delete=models.CASCADE,)
     title = models.CharField(max_length=64)
     contents = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     view_count = models.IntegerField(default=0)
     category = models.CharField(max_length=10,default='')
+    likes = models.ManyToManyField(User,related_name='like_posts',blank=True)
+    
     def __str__(self):
         return self.title
 
@@ -17,4 +19,4 @@ class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField()
     post = models.ForeignKey(to='Post',on_delete=models.CASCADE)
-    writer = models.ForeignKey(to=User,on_delete=models.CASCADE)
+    commenter = models.ForeignKey(to=User,on_delete=models.CASCADE)
