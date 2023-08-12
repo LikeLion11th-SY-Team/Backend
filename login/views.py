9b17de3df5c9be21767e5e708b40ce612f77ab8a
JSHWJ <bogum1029@gmail.com>
2023-08-13 02:36:54 +0900

    [FEAT] 인가코드 로직 구현

--- login/views.py
+++ login/views.py
@@ -3,14 +3,18 @@ from rest_framework.response import Response
 from rest_framework import status
 import requests
 from rest_framework_simplejwt.tokens import RefreshToken
-from .models import User
+from users.models import User
+# from decouple import config
 import random
 import string
+
+# KAKAO_CLIENT_ID = config('KAKAO_CLIENT_ID')
+# KAKAO_REDIRECT_URI = config('KAKAO_REDIRECT_URI')
 
 def generate_random_nickname():
     # 랜덤한 문자열을 생성하여 닉네임으로 사용
     letters = string.ascii_letters
-    random_nickname = ''.join(random.choice(letters) for i in range(10))  # 10자리 랜덤 닉네임 생성
+    random_nickname = ''.join(random.choice(letters) for i in range(6))  # 6자리 랜덤 닉네임 생성
     return random_nickname
 
 class KakaoLoginView(APIView):
@@ -24,28 +28,47 @@ class KakaoLoginView(APIView):
             return None
 
     def post(self, request):
-        access_token = request.data.get('access_token')
+        authorization_code = request.data.get('authorization_code')  # 수정: 프론트엔드에서 인가 코드 전달
 
-        user_info = self.get_user_info(access_token)
-        if user_info:
-            social_id = user_info.get('id')
+        # 카카오로 액세스 토큰 요청
+        response = requests.post('https://kauth.kakao.com/oauth/token', data={
+            'grant_type': 'authorization_code',
+            'client_id': 'dc6d77371b58fce528a75d2f7504577c' ,
+            'redirect_uri': 'http://localhost:3000/login/oauth/callback/kakao',
+            'code': authorization_code,
+        })
 
-            try:
-                user = User.objects.get(social_id=social_id)
+        if response.status_code == 200:
+            access_token = response.json().get('access_token')
+            user_info = self.get_user_info(access_token)
+            if user_info:
+                social_id = user_info.get('id')
 
-                refresh = RefreshToken.for_user(user)
-                return Response({'refresh': str(refresh), 'access': str(refresh.access_token), "msg": "로그인 성공"}, status=status.HTTP_200_OK)
+                try:
+                    user = User.objects.get(social_id=social_id)
+                    refresh = RefreshToken.for_user(user)
+                    return Response({
+                        'refresh': str(refresh),
+                        'access': str(refresh.access_token),
+                        'msg': '로그인 성공'
+                    }, status=status.HTTP_200_OK)
 
-            except User.DoesNotExist:
-                nick_name = generate_random_nickname()  # 랜덤 닉네임 생성
-                new_user = User.objects.create(
-                    nick_name=nick_name, 
-                    social_id=social_id,  # 카카오 소셜 ID 저장
-                    is_social=True,
-                )
+                except User.DoesNotExist:
+                    nick_name = generate_random_nickname()  # 랜덤 닉네임 생성
+                    new_user = User.objects.create(
+                        nick_name=nick_name,
+                        social_id=social_id,  # 카카오 소셜 ID 저장
+                        is_social=True,
+                    )
 
-                refresh = RefreshToken.for_user(new_user)
-                return Response({'refresh': str(refresh), 'access': str(refresh.access_token), "msg": "카카오 회원가입 및 로그인 성공"}, status=status.HTTP_201_CREATED)
+                    refresh = RefreshToken.for_user(new_user)
+                    return Response({
+                        'refresh': str(refresh),
+                        'access': str(refresh.access_token),
+                        'msg': '카카오 회원가입 및 로그인 성공'
+                    }, status=status.HTTP_201_CREATED)
         else:
-            return Response({"error": "유효하지 않은 액세스 토큰"}, status=status.HTTP_401_UNAUTHORIZED)
+            return Response({"error": "액세스 토큰 발급 실패"}, status=status.HTTP_400_BAD_REQUEST)
+        
+
 
--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,7 @@
 from django.urls import path
-<<<<<<< HEAD
-from .views import UserAPIView, SpartaTokenObtainPairView,SignupView
-# from .views import checkDuplicatedID,checkDuplicatedNickname
-# from .views import getNickname,getUserInfo
-=======
 from .views import UserAPIView, SpartaTokenObtainPairView,SignupView,UserinfoView
 from .views import checkDuplicatedID,checkDuplicatedNickname
 from .views import getNickname
->>>>>>> 6aa5a4b ([FEAT] User 모델 수정 및 api 추가)
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 app_name = "users"
@@ -19,15 +13,8 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/sparta/token/',SpartaTokenObtainPairView.as_view(),name='sparta_token'),
-<<<<<<< HEAD
-    # path('api/check/id/',checkDuplicatedID,name='check_id'),
-    # path('api/check/nickname/',checkDuplicatedNickname,name='check_Name'),
-    # path('api/get/nickname/',getNickname,name='get_nickname'),
-    # path('api/get/userinfo/',getUserInfo,name='get_userinfo'),
-=======
     path('api/check/id/',checkDuplicatedID,name='check_id'),
     path('api/check/nickname/',checkDuplicatedNickname,name='check_Name'),
     path('api/get/nickname/',getNickname,name='get_nickname'),
     path('api/userinfo/',UserinfoView.as_view(),name='get_userinfo'),
->>>>>>> 6aa5a4b ([FEAT] User 모델 수정 및 api 추가)
 ]
