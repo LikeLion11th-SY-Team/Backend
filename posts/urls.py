aad65ba1a64a39ea0142bd466a9904353833d726
newmin0119@gmail.com <>
2023-08-15 17:54:07 +0900

    [FEAT] 게시판 불러오기

--- posts/urls.py
+++ posts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'posts'
 urlpatterns = [
-    
+    path('', views.Posts, name='post_list'),
     path('<int:post_pk>/comments/', views.CommentView.as_view(), name='comments_create'),
     path('comments/<int:comment_pk>/delete/', views.CommentView.as_view(), name='comments_delete'),
     path('comments/<int:comment_pk>/update/', views.CommentView.as_view(), name='comments_update'),
